4dfadd0a6d0c54aab31fcc28eaf5b11264b52310
Faizaan Qureshi <qfaizaan@gmail.com>
2024-02-01 17:20:05 -0500

    add map features and city input

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,26 @@
 import { Text, View } from 'tamagui'
+import MapView from 'react-native-maps'
+import { Marker } from 'react-native-maps';
+import { StyleSheet, SafeAreaView } from 'react-native';
+
+const Paris = {
+  latitude: 25.2048,
+  longitude: 55.2708,
+};
 
 export default function TabTwoScreen() {
   return (
-    <View flex={1} alignItems='center'>
-      <Text fontSize={20}>Tab Two</Text>
-    </View>
-  )
+    <SafeAreaView>
+      <MapView style={styles.map}>
+        <Marker coordinate={Paris} />
+      </MapView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
